41ae26f366d10406e70bdf0d6e33e15ff4747f3f
labra <jelabra@gmail.com>
2013-08-14 08:41:14 +0200

    Lexical entities parsed and tested

--- src/main/scala/org/weso/parser/TurtleParser.scala
+++ src/main/scala/org/weso/parser/TurtleParser.scala
@@ -17,29 +17,58 @@ trait TurtleParser extends Positional with RegexParsers {
   override val skipWhitespace = false
 
 
-  lazy val turtleDoc = PN_PREFIX
+//  lazy val turtleDoc = PN_PREFIX
+
+  lazy val RDFLiteral = string ~ opt(LANGTAG.r | "^^" ~ iri) 
   
-  lazy val PN_CHARS_BASE_STR =
- 	"[a-zA-Z\\u00C0-\\u00D6\\u00D8-\\u00F6" +
- 	"\\u00F8-\\u02FF\\u0370-\\u037D\\u037F-\\u1FFF" +
- 	"\\u200C-\\u200D\\u2070-\\u218F\\u2C00-\\u2FEF" +
- 	"\\u3001-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFFD" + 
-   	"\\x{10000}-\\x{EFFFF}]" 
- lazy val PN_CHARS_U_STR    = PN_CHARS_BASE_STR + "|_"
- lazy val PN_CHARS_STR		= PN_CHARS_U_STR + "|-|[0-9]|\\u00B7|[\\u0300-\u036F]|[\\u203F-\\u2040]" 
+  lazy val BooleanLiteral = "true" | "false" 
+  lazy val string = STRING_LITERAL_LONG_QUOTE.r | STRING_LITERAL_LONG_SINGLE_QUOTE.r | 
+  					STRING_LITERAL_QUOTE.r | STRING_LITERAL_SINGLE_QUOTE.r | 
+  					failure("string expected")
+  					
+  lazy val iri = IRIREF.r | PrefixedName | failure("iri expected")
+  lazy val PrefixedName = PNAME_LN.r | PNAME_NS.r | failure("Prefixed Name expected")
+  lazy val BlankNode	= BLANK_NODE_LABEL.r | ANON.r | failure("Blank Node expected")
   
- lazy val PN_CHARS_BASE				= PN_CHARS_BASE_STR.r
- lazy val PN_CHARS_U				= PN_CHARS_U_STR.r
- lazy val PN_CHARS					= PN_CHARS_STR.r
- 
- lazy val PN_PREFIX_STR				= PN_CHARS_BASE_STR + "((" + PN_CHARS_STR + "|\\." + ")*" + PN_CHARS_STR + ")?"
- lazy val PN_PREFIX					= PN_PREFIX_STR.r
- 
- 
- lazy val PLX						= PERCENT | PN_LOCAL_ESC
- lazy val PERCENT : Parser[String]	= "%" ~> HEX ~ HEX ^^ { case h1 ~ h2 => "%" + h1 + h2 } // To convert: Integer.parseInt(h1 + h2,16) } 
- lazy val HEX 						= """[0-9A-Fa-f]""".r
- lazy val PN_LOCAL_ESC 				= """[\\][_~\.\-!$&'\(\)*+,;=/?#@%]""".r 
+  lazy val IRIREF		= "<([^\\u0000-\\u0020<>\\\\\"{}\\|\\^`\\\\]|" + UCHAR + ")*>"
+  lazy val PNAME_NS		= "(" + PN_PREFIX + ")?:"
+  lazy val PNAME_LN 	= PNAME_NS + PN_LOCAL
+  lazy val BLANK_NODE_LABEL = "_:(" + PN_CHARS_U + "|[0-9])(("+ PN_CHARS + "|\\.)*" + PN_CHARS + ")?"
+  lazy val LANGTAG		= "@[a-zA-Z]+(-[a-zA-Z0-9]+)*"
+  lazy val INTEGER 		= "[+-]?[0-9]+"
+  lazy val DECIMAL		= "[+-]?[0-9]*\\.[0-9]+"
+  lazy val DOUBLE		= "[+-]?([0-9]+\\.[0-9]*" + EXPONENT + "|\\.[0-9]+" + EXPONENT + "|[0-9]+" + EXPONENT + ")"
+  lazy val EXPONENT		= "[eE][+-]?[0-9]+"
+  
+  lazy val STRING_LITERAL_QUOTE 
+  						= "\"([^\\u0027\\u005C\\u000A\\u000D]|" + ECHAR + "|" + UCHAR + ")*\""  
+
+  lazy val STRING_LITERAL_SINGLE_QUOTE 
+  						= "'([^\\u0027\\u005C\\u000A\\u000D]|" + ECHAR + "|" + UCHAR + ")*'"  
+
+  lazy val STRING_LITERAL_LONG_SINGLE_QUOTE 
+  						= "'''(('|'')?[^']|" + ECHAR + "|" + UCHAR + ")*'''"  
+
+  lazy val STRING_LITERAL_LONG_QUOTE 
+  						= "\"\"\"((\"|\"\")?[^\"]|" + ECHAR + "|" + UCHAR + ")*\"\"\""  
+  lazy val UCHAR 		= "\\\\u" + HEX + HEX + HEX + HEX + "|" + "\\\\U" + HEX + HEX + HEX + HEX + HEX + HEX
+  lazy val ECHAR 		= "\\\\[tbnrf\"]" 
+  lazy val WS 			= """\u0020|\u0009|\u000D|\u000A"""
+  lazy val ANON 		= "\\[(" + WS + ")*\\]"  
+  lazy val PN_CHARS_BASE =
+ 	"""[a-zA-Z\u00C0-\u00D6\u00D8-\u00F6""" +
+ 	"""\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF""" +
+ 	"""\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF""" +
+ 	"""\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD""" + 
+   	"""\x{10000}-\x{EFFFF}]""" 
+ lazy val PN_CHARS_U    = PN_CHARS_BASE + "|_"
+ lazy val PN_CHARS		= PN_CHARS_U + """|\-|[0-9]|\u00B7|[\u0300-\u036F]|[\u203F-\u2040]""" 
+ lazy val PLX			= PERCENT + "|" + PN_LOCAL_ESC
+ lazy val PN_PREFIX     = PN_CHARS_BASE + "((" + PN_CHARS + "|\\.)*" + PN_CHARS + ")?" 
+ lazy val PN_LOCAL		= "(" + PN_CHARS_U + "|:|[0-9]|" + PLX + ")((" + PN_CHARS + "|\\.|:|" + PLX + ")*(" + PN_CHARS + "|:|" + PLX + "))?"     
+ lazy val PERCENT 		= "%" + HEX + HEX 
+ lazy val HEX 			= """[0-9A-Fa-f]"""  
+ lazy val PN_LOCAL_ESC 	= """[\\][_~\.\-!$&'\(\)*+,;=/?#@%]"""
  
 
 }
--- src/test/scala/org/weso/parser/TurtleW3cTestsSuite.scala
+++ src/test/scala/org/weso/parser/TurtleW3cTestsSuite.scala
@@ -14,57 +14,306 @@ import scala.util.parsing.input.CharArrayReader
 
 class TurtleW3cTestsSuite extends TurtleParser with FunSpec with ShouldMatchers {
 
-  describe("Turtle Parser") {
+  describe("Turtle Lexer") {
    val parser=TurtleParser
 
+   describe("RDFLiteral") {
+     val p = parser.RDFLiteral
+     shouldParseGen(p,"\"John\"")
+	 shouldParseGen(p,"\"123\"^^<xsd:integer>")
+	 shouldParseGen(p,"\"\"\"John Doe\"\"\"")
+	 shouldParseGen(p,"one@es")
+	 shouldParseGen(p,"one")
+	 shouldParseGen(p,"one two")
+	 shouldParseGen(p,".")
+	 shouldNotParse(p,".")
+   }
+
+   describe("BooleanLiteral") {
+     val p = parser.BooleanLiteral
+     shouldParse(p,"true")
+	 shouldParse(p,"false")
+	 shouldNotParse(p,"tres")
+	 shouldNotParse(p,"one")
+   }
+
+	 
+	 describe("string") {
+     val p = parser.string
+     shouldParse(p,"\"hi\"")
+	 shouldParse(p,"'Hi'")
+	 shouldParse(p,"'''Hi'John'''")
+	 shouldNotParse(p,"3.2")
+   }
+
+   describe("iri") {
+     val p = parser.iri
+     shouldParse(p,":a")
+	 shouldParse(p,"a:b")
+	 shouldParse(p,"p:")
+	 shouldParse(p,"<http://a.com>")
+	 shouldParse(p,"<a.com>")
+	 shouldNotParse(p,"3.2")
+   }
+
+   describe("PrefixedName") {
+     val p = parser.PrefixedName
+     shouldParse(p,":a")
+	 shouldParse(p,"a:b")
+	 shouldParse(p,"p:")
+	 shouldNotParse(p,"<a>")
+   }
+
+   describe("BlankNode") {
+     val p = parser.BlankNode
+     shouldParse(p,"_:1")
+	 shouldParse(p,"_:a")
+	 shouldParse(p,"[]")
+	 shouldNotParse(p,"<a>")
+   }
+
+   describe("IRIREF") {
+     val p = IRIREF.r
+     shouldParse(p,"<>")
+	 shouldParse(p,"<hi>")
+	 shouldParse(p,"<http://pepe.com>")
+	 shouldParse(p,"<urn://kiko-uno>")
+	 shouldNotParse(p,"<a>.")
+	 shouldNotParse(p,"<a\"b>")
+	 shouldNotParse(p,"<a{a}b>")
+   }
+
+   describe("PNAME_NS") {
+     val p = PNAME_NS.r
+     shouldParse(p,"a:")
+	 shouldParse(p,":")
+	 shouldParse(p,"año:")
+	 shouldNotParse(p,"a:b")
+   }
+
+   describe("PNAME_LN") {
+     val p = PNAME_LN.r
+     shouldParse(p,"a:b")
+	 shouldParse(p,"a:aa")
+	 shouldParse(p,"a:año")
+	 shouldNotParse(p,"a:año ")
+   }
+
+   describe("BLANK_NODE_LABEL") {
+     val p = BLANK_NODE_LABEL.r
+	 shouldParse(p,"_:1")
+	 shouldParse(p,"_:aa")
+	 shouldParse(p,"_:año")
+// fails?	 shouldParse(p,"_:1.2")
+	 shouldParse(p,"_:1.2a")
+	 shouldParse(p,"_:1.a")
+	 shouldNotParse(p,"_:1 a .")
+   }
+
+   describe("LANGTAG") {
+     val p = LANGTAG.r
+	 shouldParse(p,"@es")
+	 shouldParse(p,"@es-am")
+	 shouldParse(p,"@am-am2-23")
+	 shouldNotParse(p,"es")
+	 shouldNotParse(p,"@12")
+	 shouldNotParse(p,"@año")
+   }
+
+   describe("INTEGER") {
+     val p = INTEGER.r
+	 shouldParse(p,"2")
+	 shouldParse(p,"-3")
+	 shouldParse(p,"-33")
+	 shouldNotParse(p,"3.4")
+	 shouldNotParse(p,"334.")
+	 shouldNotParse(p,"-3.34E+98")
+   }
+
+	 describe("DECIMAL") {
+     val p = DECIMAL.r
+	 shouldParse(p,"2.34")
+	 shouldParse(p,"-3.0")
+	 shouldParse(p,"-3.3")
+	 shouldNotParse(p,"3")
+	 shouldNotParse(p,"334")
+	 shouldNotParse(p,"-3.34E+98")
+	 shouldNotParse(p,"-334")
+   }
+
+   describe("DOUBLE") {
+     val p = DOUBLE.r
+	 shouldParse(p,"2.34e9876")
+	 shouldParse(p,"-3E+98")
+	 shouldParse(p,"-3.34E+98")
+	 shouldNotParse(p,"-3.34EE+98")
+	 shouldNotParse(p,"-3.34E +98")
+   }
+
+   describe("EXPONENT") {
+     val p = EXPONENT.r
+	 shouldParse(p,"e9876")
+	 shouldParse(p,"E+98")
+	 shouldParse(p,"E-98")
+	 shouldParse(p,"e-98")
+	 shouldNotParse(p,"e++")
+	 shouldNotParse(p,"e+a")
+	 shouldNotParse(p,"E+-98")
+   }
+
+   describe("STRING_LITERAL_QUOTE") {
+     val p = STRING_LITERAL_QUOTE.r
+	 shouldParse(p,"\"Hello\"")
+	 shouldParse(p,"\"\\u0123\t\\u00AF\"")
+	 shouldNotParse(p,"\"Ho\"la\"")
+   }
+
+   describe("STRING_LITERAL_SINGLE_QUOTE") {
+     val p = STRING_LITERAL_SINGLE_QUOTE.r
+	 shouldParse(p,"'Hello'")
+	 shouldParse(p,"'\\u0123'")
+	 shouldNotParse(p,"'Ho'la'")
+   }
+
+   describe("STRING_LITERAL_LONG_SINGLE_QUOTE") {
+     val p = STRING_LITERAL_LONG_SINGLE_QUOTE.r
+	 shouldParse(p,"'''Hello'''")
+	 shouldParse(p,"'''\\u0123'''")
+	 shouldParse(p,"'''H e l l o . '''")
+	 shouldParse(p,"'''Hi 'John' . '''")
+	 shouldParse(p,"'''Hi'John'''")
+	 shouldParse(p,"'''Hi \"John\" . '''")
+	 shouldParse(p,"'''Hi \"\"John\"\" . '''")
+	 shouldNotParse(p,"\"\"\"Hi \"\"\"John\"\" . \"\"\"")
+   }
+
+   describe("STRING_LITERAL_LONG_QUOTE") {
+     val p = STRING_LITERAL_LONG_QUOTE.r
+	 shouldParse(p,"\"\"\"Hello\"\"\"")
+	 shouldParse(p,"\"\"\"\\u0123\"\"\"")
+	 shouldParse(p,"\"\"\"H e l l o . \"\"\"")
+	 shouldParse(p,"\"\"\"Hi \"John\" . \"\"\"")
+	 shouldParse(p,"\"\"\"Hi \'John\' . \"\"\"")
+	 shouldParse(p,"\"\"\"Hi \"\"John\"\" . \"\"\"")
+	 shouldNotParse(p,"\"\"\"Hi \"\"\"John\"\" . \"\"\"")
+	 
+   }
+   
+   describe("UCHAR") {
+     val p = UCHAR.r
+	 shouldParse(p,"\\u0123")
+	 shouldParse(p,"\\u01AF")
+	 shouldParse(p,"\\U0123AD")
+	 shouldNotParse(p,"\\x0123AD")
+	 shouldNotParse(p,"\\u0123AD")
+	 shouldNotParse(p,"\\U0123")
+	 shouldNotParse(p,"\\u012H")
+   }
+
+   describe("ECHAR") {
+     val p = ECHAR.r
+	 shouldParse(p,"\\t")
+	 shouldParse(p,"\\b")
+	 shouldParse(p,"\\n")
+	 shouldParse(p,"\\r")
+	 shouldParse(p,"\\f")
+	 shouldParse(p,"\\\"")
+	 shouldNotParse(p,"a")
+   }
+
+   
+   describe("WS") {
+     val p = WS.r
+	 shouldParse(p," ")
+	 shouldNotParse(p,"a")
+   }
+
+   describe("ANON") {
+     val p = ANON.r
+	 shouldParse(p,"[]")
+	 shouldParse(p,"[ ]")
+	 shouldParse(p,"[ \t]")
+	 shouldParse(p,"[ \n]")
+	 shouldNotParse(p,"[a]")
+   }
+
    describe("PN_CHARS_BASE") {
-	 shouldParse(parser.PN_CHARS_BASE,"A")
-	 shouldParse(parser.PN_CHARS_BASE,"\u2088")
-	 shouldNotParse(parser.PN_CHARS_BASE, "\u2001")
+     val p = PN_CHARS_BASE.r
+	 shouldParse(p,"A")
+	 shouldParse(p,"Ñ")
+	 shouldParse(p,"\u2088")
+	 shouldNotParse(p, "\u2001")
    }
 
    describe("PN_CHARS") {
-     val p = parser.PN_CHARS
+     val p = PN_CHARS.r
 	 shouldParse(p,"A")
 	 shouldParse(p,"-")
+	 shouldParse(p,"7")
+	 shouldParse(p,"\u00B7")
 	 shouldNotParse(p, ":")
    }
 
 
    describe("PN_PREFIX") {
-     val p = parser.PN_PREFIX
+     val p = PN_PREFIX.r
 	 shouldParse(p,"A")
 	 shouldParse(p,"a.o")
+	 shouldParse(p,"a..o")
+	 shouldParse(p,"a.ñ.o")
+	 shouldParse(p,"a.x.o")
 //	 shouldParse(p,"a-1")
+//	 shouldParse(p,"a--1")
 	 shouldParse(p,"año")
      shouldParse(p,"AA")
      shouldParse(p, "b.a")
 	 shouldNotParse(p, "b:a")
    }
 
+   
+   describe("PN_LOCAL") {
+     val p = PN_LOCAL.r
+	 shouldParse(p,"Año")
+	 shouldParse(p,":a")
+	 shouldParse(p,":89")
+	 shouldParse(p,":año_88")
+	 shouldParse(p,":año:88")
+	 shouldNotParse(p,":año88 .")
+	 shouldNotParse(p,":año88,")
+   }
+
    describe("PLX") {
-	 shouldParse(parser.PLX,"%12")
-	 shouldParse(parser.PLX,"\\-")
+     val p = PLX.r
+	 shouldParse(p,"%12")
+	 shouldParse(p,"\\-")
    }
 
    describe("PERCENT") {
-	 shouldParse(parser.PERCENT,"%12")
-	 shouldParse(parser.PERCENT,"%9F")
-	 shouldParse(parser.PERCENT,"%FF")
-	 shouldNotParse(parser.PERCENT, "AAA")
-	 shouldNotParse(parser.PERCENT, "%%")
-	 shouldNotParse(parser.PERCENT, "%GH")
+     val p = PERCENT.r
+	 shouldParse(p,"%12")
+	 shouldParse(p,"%9F")
+	 shouldParse(p,"%FF")
+	 shouldNotParse(p, "AAA")
+	 shouldNotParse(p, "%%")
+	 shouldNotParse(p, "%GH")
    }
 
    
    describe("PN_LOCAL_ESC") {
-     shouldParse(parser.PN_LOCAL_ESC,"\\-")
-     shouldParse(parser.PN_LOCAL_ESC,"\\!")
-     shouldNotParse(parser.PN_LOCAL_ESC,"a")
+     val p = PN_LOCAL_ESC.r
+     shouldParse(p,"\\-")
+     shouldParse(p,"\\!")
+     shouldNotParse(p,"a")
    }
 
    def shouldParse(p:TurtleParser.Parser[String], s : String) {
      shouldParseGeneric(p,s,s)
+   }
+
+   def shouldParseGen[A](p:TurtleParser.Parser[A], s : String) {
+    it("Should parse \"" + s + "\"") {
+      parser.parseAll(p,s).successful === true
+    }
    }
 
    def shouldParseGeneric[A](p:TurtleParser.Parser[A], s : String, a : A) {
